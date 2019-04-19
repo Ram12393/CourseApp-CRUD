205e570ac3eb1e8836855cb15cd102aa25c6b8ce
Ram <ram.mohan@refixd.com>
2019-04-19 06:38:58 +0530

    populate the course schema in student schema

--- controllers/student.controller.js
+++ controllers/student.controller.js
@@ -4,8 +4,9 @@ const Course = require('../models/course.model');
 exports.createStudent = async (req, res) => {
     try {
         const student = new Student(req.body);
-        const result = await student.save().then(t =>
-            Student.findById(student._id).populate('course_name')); // Pass new ID and then populate the "course_name"
+        const result = await student.save();
+        // const result = await student.save().then(t =>
+        //     Student.findById(student._id).populate('course_name')); // Pass new ID and then populate the "course_name"
         if (!result) res.send('something went wrong');
         console.log(result);
         res.send(result)
@@ -16,7 +17,7 @@ exports.createStudent = async (req, res) => {
 
 exports.getAllStudents = async (re, res) => {
     try {
-        const students = await Student.find();
+        const students = await Student.find().populate('course_name'); //pass key name which we need to populate
         if (!students) res.send("No records found");
         res.send(students);
     } catch (e) {
